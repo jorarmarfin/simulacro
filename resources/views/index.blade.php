2f44f3aba02c8e5450e424cb6242ceb00892bf15
Luis Mayta <luis.mayta@gmail.com>
2017-03-20 11:49:21 -0500

    Layout 4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,81 +4,59 @@
 {!! Alert::render() !!}
 @include('alerts.errors')
 <div class="row">
-    <div class="col-md-3">
-        <a class="dashboard-stat dashboard-stat-v2 blue" href="{{ route('datos.index') }}">
-            <div class="visual">
-                <i class="fa fa-users"></i>
+    <div class="col-md-12">
+    <!-- BEGIN PORTLET-->
+    <div class="portlet light ">
+        <div class="portlet-title margin-bottom-10">
+            <div class="caption caption-md font-red-sunglo">
+                <span class="caption-subject theme-font bold uppercase">Bienvenido</span>
             </div>
-            <div class="details">
-                <div class="number">
-                    <span >DATOS</span>
-                </div>
-                <div class="desc"> Datos Personales </div>
-            </div>
-        </a>
+        </div>
+        <div class="portlet-body overflow-h">
+            Indicaciones
+        </div>
     </div>
-    <div class="col-md-3 ">
-        <a class="dashboard-stat dashboard-stat-v2 red" href="{{ route('pagos.index') }}">
-            <div class="visual">
-                <i class="fa fa-dollar"></i>
-            </div>
-            <div class="details">
-                <div class="number">
-                    <span >PAGO</span></div>
-                <div class="desc"> Formatos de pagos </div>
-            </div>
-        </a>
-    </div>
-    <div class="col-md-3 ">
-        <a class="dashboard-stat dashboard-stat-v2 green" href="{{ route('ficha.index') }}">
-            <div class="visual">
-                <i class="fa fa-file-pdf-o"></i>
-            </div>
-            <div class="details">
-                <div class="number">
-                    <span >FICHA</span>
-                </div>
-                <div class="desc"> Ficha de inscripción </div>
-            </div>
-        </a>
-    </div>
-    <div class="col-lg-3 ">
-        <a class="dashboard-stat dashboard-stat-v2 purple" href="{{ route('resultados.index') }}">
-            <div class="visual">
-                <i class="fa fa-globe"></i>
-            </div>
-            <div class="details">
-                <div class="number">
-                    <span >RESULTADOS</span> </div>
-                <div class="desc"> Resultados de tu evaluación </div>
-            </div>
-        </a>
-    </div>
+    <!-- END PORTLET-->
 </div>
-
+</div><!--row-->
 <div class="row">
     <div class="col-md-12">
         <div class="mt-element-step">
             <div class="row step-background">
-                <div class="col-md-4 bg-grey-steel mt-step-col">
+                <a href="{{ route('datos.index') }}">
+                <div class="col-md-3 bg-grey-steel mt-step-col">
                     <div class="mt-step-number">1</div>
-                    <div class="mt-step-title uppercase font-grey-cascade">Purchase</div>
-                    <div class="mt-step-content font-grey-cascade">Purchasing the item</div>
+                    <div class="mt-step-title uppercase font-grey-cascade">Datos</div>
+                    <div class="mt-step-content font-grey-cascade">Datos Personales</div>
                 </div>
-                <div class="col-md-4 bg-grey-steel mt-step-col active">
+                </a>
+                <a href="{{ route('pagos.index') }}">
+                <div class="col-md-3 bg-grey-steel mt-step-col active">
                     <div class="mt-step-number">2</div>
-                    <div class="mt-step-title uppercase font-grey-cascade">Payment</div>
-                    <div class="mt-step-content font-grey-cascade">Complete your payment</div>
+                    <div class="mt-step-title uppercase font-grey-cascade">Pago</div>
+                    <div class="mt-step-content font-grey-cascade">Formatos de pagos</div>
                 </div>
-                <div class="col-md-4 bg-grey-steel mt-step-col">
+                </a>
+                <a href="{{ route('ficha.index') }}">
+                <div class="col-md-3 bg-grey-steel mt-step-col">
                     <div class="mt-step-number">3</div>
-                    <div class="mt-step-title uppercase font-grey-cascade">Deploy</div>
-                    <div class="mt-step-content font-grey-cascade">Receive item integration</div>
+                    <div class="mt-step-title uppercase font-grey-cascade">Ficha</div>
+                    <div class="mt-step-content font-grey-cascade">Ficha de inscripción</div>
                 </div>
+                </a>
+                <a href="{{ route('resultados.index') }}">
+                <div class="col-md-3 bg-grey-steel mt-step-col active">
+                    <div class="mt-step-number">4</div>
+                    <div class="mt-step-title uppercase font-grey-cascade">Resultado</div>
+                    <div class="mt-step-content font-grey-cascade">Resultados de tu evaluación</div>
+                </div>
+                </a>
             </div>
         </div>
     </div><!--span-->
 </div><!--row-->
+
+
 @stop
 
 
