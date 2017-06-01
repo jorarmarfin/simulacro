1e5d76b0c6782af5406efb2d3154541e96b3fda1
Luis Mayta <luis.mayta@gmail.com>
2017-06-01 15:00:58 -0500

    Correccion al save de datatable

--- resources/views/admin/postulantes/index.blade.php
+++ resources/views/admin/postulantes/index.blade.php
@@ -89,7 +89,7 @@ $('.Postulantes').DataTable({
         "search": "Buscar Postulante :",
         "lengthMenu": "_MENU_ registros"
     },
-    stateSave: true
+    stateSave: true,
     responsive: true,
     dom: "<'row' <'col-md-12'B>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
     buttons: [
